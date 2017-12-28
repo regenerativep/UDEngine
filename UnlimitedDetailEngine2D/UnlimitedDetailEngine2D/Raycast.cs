ebf8eea696385fb80356df5d9c803eeb47218344
regenerativep <regeneralp@gmail.com>
2017-12-28 16:08:49 -0500

    +2d version
    added this, will convert to 3d later
    current issue has something to do with the order of which raycasts find the nodes of the quadtree and some endless recursive loop i cant figure out at the moment

---
+++ UnlimitedDetailEngine2D/UnlimitedDetailEngine2D/Raycast.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace UnlimitedDetailEngine2D
+{
+    public class Raycast
+    {
+        public static float MAX_DISTANCE = 16384;
+        public Vector2 Position { get; set; }
+        public Vector2 Direction
+        {
+            get
+            {
+                return NormalizedDirection * MAX_DISTANCE;
+            }
+            set
+            {
+                NormalizedDirection = value;
+                NormalizedDirection.Normalize();
+            }
+        }
+        public Vector2 NormalizedDirection { get; private set; }
+        public Raycast(Vector2 pos, Vector2 dir)
+        {
+            Position = pos;
+            NormalizedDirection = dir;
+        }
+    }
+}
